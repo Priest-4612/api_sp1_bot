dca6333ccd126933c1fb8d9363c413d574be2066
priest4612 <prie4612@gmail.com>
2021-08-04 02:54:58 +0300

    Исправил замечания

--- homework.py
+++ homework.py
@@ -7,117 +7,108 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-from constants import Exit_Code, Http_Status_Code
-
 load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
+MESSAGE_STATUS = (
+    'У вас проверили работу '
+    '"{homework_name}"!\n\n{verdict}'
+)
+VERDICTS = {
+    'reviewing': 'Работа взята в ревью.',
+    'approved': 'Ревьюеру всё понравилось, работа зачтена!',
+    'rejected': 'К сожалению, в работе нашлись ошибки.'
+}
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+_log_folder = os.path.join(base_dir, 'logs')
 _log_format = (
     '%(asctime)s - [%(levelname)s] -  %(name)s - '
     '(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s'
 )
 
-_log_folder = r'./logs'
-
-
-def get_file_handler(path, name):
-    file_handler = logging.FileHandler(
-        f'{path}/{name}', mode='a', encoding='utf8'
-    )
-    file_handler.setLevel(logging.WARNING)
-    file_handler.setFormatter(logging.Formatter(_log_format))
-    return file_handler
-
-
-def get_stream_handler():
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
-    stream_handler.setFormatter(logging.Formatter(_log_format))
-    return stream_handler
-
-
-def get_logger(name):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(get_file_handler(_log_folder, name))
-    logger.addHandler(get_stream_handler())
-    return logger
-
-
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
-logger = get_logger(__name__)
-
-
-class IsRequestError(Exception):
-    pass
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=_log_format
+)
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(
+    f'{_log_folder}/{__name__}', mode='a', encoding='utf8'
+)
+file_handler.setLevel(logging.WARNING)
+file_handler.setFormatter(logging.Formatter(_log_format))
+stream_handler = logging.StreamHandler()
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению, в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    verdict = VERDICTS[homework['status']]
+    return MESSAGE_STATUS.format(homework_name=homework_name, verdict=verdict)
+
+
+class RequestsError(Exception):
+    pass
 
 
 def get_homeworks(current_timestamp):
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
 
     try:
-        homework_statuses = requests.get(url, headers=headers, params=payload)
-        response_status = homework_statuses.status_code
-
-        if response_status != Http_Status_Code['OK']['code']:
-            raise IsRequestError
-
-        return homework_statuses.json()
-
-    except IsRequestError:
-        print('Ошибка при работе с API сервиса Практикум.Домашка!')
-        for key, item in Http_Status_Code.items():
-            if item['code'] == response_status:
-                message = (
-                    'IsRequestError: Ошибка запроса. '
-                    f'status code: { item["code"] }. '
-                    f'message: { item["message"] }. '
-                )
-                logger.error(message)
-                send_message(message)
-
-        sys.exit(Exit_Code['ERROR'])
+        homework_statuses = requests.get(URL, headers=HEADERS, params=payload)
+        homeworks_json = homework_statuses.json()
+        keys = homeworks_json.keys()
+        if 'error' in keys or 'code' in keys:
+            raise RequestsError({'message': f'code: {homeworks_json["code"]}'})
+        return homeworks_json
+    except RequestsError:
+        message = (
+            f'Ошибка запроса!'
+            f'\nPraktikum API error: {homeworks_json} '
+            f'\nrequest: url: {URL}, params: {payload}'
+        )
+        logger.error(message)
+        send_message(message)
+        sys.exit(1)
 
 
 def send_message(message):
-    logger.info(f'Сообщение отправлено. Текст сообщения: {message}')
-    return bot.send_message(CHAT_ID, message)
+    bot.send_message(CHAT_ID, message)
+    logger.info(
+        f'Сообщение отправлено. Текст в сообщении: {message}'
+    )
+    return
 
 
 def main():
     logger.debug('Старт приложения. Бот запущен!')
-    current_timestamp = int(time.time())  # Начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
         try:
-            homeworks = get_homeworks(current_timestamp)['homeworks']
-            for hw in homeworks:
-                message = parse_homework_status(hw)
+            homeworks_json = get_homeworks(current_timestamp)
+            current_timestamp = homeworks_json['current_date']
+            homework = homeworks_json['homeworks']
+            if len(homework) != 0:
+                message = parse_homework_status(homework[0])
                 send_message(message)
 
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
+            time.sleep(20 * 60)
 
-        except Exception as e:
-            logger.error(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
+        except Exception as error:
+            message = f'Произошла ошибка: {error}'
+            logger.error(message, exc_info=True)
+            send_message(message)
+            time.sleep(20 * 60)
 
 
 if __name__ == '__main__':
